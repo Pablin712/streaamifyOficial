6442da81291adaf270a07354402c5afe1495942a
Pablo Dario Jimenez Elizalde <pdjimeneze@utn.edu.ec>
2024-11-20 10:52:52 -0500

    probar cambio realizado
    Andres Rincon se une y verifica la conexion y la prueba
    
    Co-Authored-By: Pablo Dario Jimenez Elizalde <176194374+pablin712@users.noreply.github.com>

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.plantilla')
 @section('title','Curso'.$curso)
 @section('content')
-<h1>Bienvenido al curso: {{$curso}}</h1>
+<h1>Bienvenido al curso Andres Rincon, el curso es: {{$curso}}</h1>
 @endsection
