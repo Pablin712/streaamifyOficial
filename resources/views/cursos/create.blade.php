919c0f1d0a8873ee30551cfbdd4cf8f510f712b1
Pablo Dario Jimenez Elizalde <pdjimeneze@utn.edu.ec>
2024-11-20 16:25:10 -0500

    add bootstraps plantillas
    solo agregué plantillas

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -1,5 +1,6 @@
-@extends('layouts.plantilla')
+@extends('layouts.dashboard')
 @section('title','Create cursos')
 @section('content')
     <h1>En esta pagina podrás crear un curso</h1>
 @endsection
+@section('section title','Texto modificado ahora')
