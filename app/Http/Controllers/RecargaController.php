d6d30aeadcd1d13547479610c22133b9ade09d97
Andres Rincon <jarincong@utn.edu.ec>
2025-01-27 18:29:29 -0500

    Recargas validadas con el id del cliente

--- app/Http/Controllers/RecargaController.php
+++ app/Http/Controllers/RecargaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Banco;
 use App\Models\Recarga;
+use Illuminate\Support\Facades\Auth;
+
 class RecargaController extends Controller
 {
     public function recargarSaldo()
@@ -12,19 +14,22 @@ class RecargaController extends Controller
         $bancos = Banco::all(); // Obtiene todos los bancos
         return view('shopping.recargas', compact('bancos'));
     }
+
     public function procesarRecarga(Request $request)
     {
         $request->validate([
             'idban' => 'required|exists:bancos,idban',
-            'numcomprobante' => 'required|string|max:255',
+            'numcomprobante' => 'required|string|max:255|unique:recargas,numcomprobante',
             'valor' => 'required|numeric|min:1',
             'foto' => 'required|image|max:2048',
+        ], [
+            'numcomprobante.unique' => 'Comprobante ya enviado',
         ]);
 
         $fotoPath = $request->file('foto')->store('comprobantes', 'public');
 
         Recarga::create([
-            'idcli' => $request->idcli,
+            'idcli' => Auth::guard('cliente')->user()->idcli,
             'idban' => $request->idban,
             'numcomprobante' => $request->numcomprobante,
             'valor' => $request->valor,
@@ -32,6 +37,6 @@ class RecargaController extends Controller
             'idestado' => 1, // Estado inicial, por ejemplo "Pendiente"
         ]);
 
-        return redirect()->route('recargar.saldo.view')->with('success', '¡Recarga enviada con éxito!');
+        return redirect()->route('recargar.index')->with('success', '¡Recarga enviada con éxito, porfavor tener paciencia!');
     }
 }
