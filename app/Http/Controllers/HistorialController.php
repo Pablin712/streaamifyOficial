d044da7a61b5ba3d22843df5d2ce59298dc72026
Andres Rincon <jarincong@utn.edu.ec>
2025-01-27 18:00:54 -0500

    bloqueo a acceso a historial

--- app/Http/Controllers/HistorialController.php
+++ app/Http/Controllers/HistorialController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Historial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HistorialController extends Controller
 {
     public function show()
     {
+        $this->authorizeRole(['administrador']);
         $historial = Historial::orderBy('fecha', 'desc')->get();
         return view('historial.index', compact('historial'));
+    }
+    private function authorizeRole(array $roles)
+    {
+        $userRole = Auth::user()->idrol;
+
+        if (!in_array($userRole, $roles)) {
+            // Redirigir a la vista anterior con una alerta
+            return redirect()->back()->with('error', 'No tienes permisos para realizar esta acción.')->send();
+        }
     }
 }
