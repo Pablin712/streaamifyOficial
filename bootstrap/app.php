48089ab55a2023babbde5073ec1aa8f705e28c44
Pablo Dario Jimenez Elizalde <pdjimeneze@utn.edu.ec>
2026-05-28 11:52:23 -0500

    definir url de redireccionamiento

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,16 +7,12 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
         using: function () {
-            // Registrar rutas de la API
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            // Registrar rutas web (si las tienes)
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         },
